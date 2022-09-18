51673df2d944e8c49d4cb3f81da852dd935e130c
sonotype <contact.marco.accardi@gmail.com>
2022-09-18 13:34:07 +0200

    setup initial project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Header/Header";
+import Gallery from "./pages/Gallery";
+import Slideshow from "./pages/Slideshow";
 
 function App() {
-  return <div className='App'>HOME</div>;
+  return (
+    <>
+      <Router>
+        <Header />
+        {/* LIGHTBOX */}
+        {/* ANIMATION FRAMER MOTION */}
+        <Route exact path='/' component={<Gallery />} />
+        <Route exact path='/slideshow' component={<Slideshow />} />
+      </Router>
+    </>
+  );
 }
 
 export default App;
